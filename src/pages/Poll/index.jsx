934f46369db92a30a531d75bf44b84e729c269e9
Ch1ngY1Y1O9z9 <l123050891@gmail.com>
2022-03-21 23:03:51 +0800

    update

--- src/pages/Poll/index.jsx
+++ src/pages/Poll/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-// import { Route, Routes, useParams, Link } from 'react-router-dom'
+import { Route, Routes, useParams, Link } from 'react-router-dom'
 
 import PagesTitle from '../../components/PagesTitle'
 
@@ -7,11 +7,11 @@ import PagesTitle from '../../components/PagesTitle'
 import { connect } from 'react-redux'
 
 // 引入React Intl切換語系
-// import { FormattedMessage } from 'react-intl'
+import { FormattedMessage } from 'react-intl'
 
 function Poll(props) {
 
-    const { light, pageName } = props
+    const { light, pageName, lang } = props
 
     const [data, setData] = useState([]) //取得頁面資料
 
@@ -36,7 +36,7 @@ function Poll(props) {
         <section className={`min-h-screen pt-12 ${light ? 'bg-white' : 'bg-black'}`}>
             <PagesTitle data={{ title: 'POLL', description: 'Will announce new poll before next stream(maybe?).', pageName, light }} />
 
-            <div className="w-full flex flex-wrap text-gray-800">
+            <div className={`w-full flex flex-wrap ${light ? 'text-gray-800' : 'text-gray-200'}`}>
                 {
                     data.map((item, index) => {
                         return (
@@ -45,21 +45,21 @@ function Poll(props) {
                                 <div
                                     className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center relative z-10 text-red-500 text-2xl font-bold">
                                     {index + 1}</div>
-                                <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row py-10">
+                                <Link to={`/${lang}/Wrestlers/Profile/${item.name_short}`} className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row py-10">
                                     <div
                                         className="flex-shrink-0 w-24 h-24 rounded-full inline-flex items-center justify-center overflow-hidden">
                                         <img width="100%" src={item.avatar} alt={item.name_short} />
                                     </div>
                                     <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
                                         <h2 className="font-medium title-font mb-1 text-2xl font-bold text-center sm:text-left">
-                                            {item.name_short}
+                                        <FormattedMessage id={`app.Characters.${item.name_short}`} defaultMessage={item.name_short} />
                                         </h2>
                                         <p className="leading-relaxed text-xl">
-                                            Total Point: {item.point} <br />
-                                            Total Votes: {item.vote}
+                                        <FormattedMessage id={`app.Poll.point`} defaultMessage='Total Point' />: {item.point} <br />
+                                        <FormattedMessage id={`app.Poll.votes`} defaultMessage='Total Votes' />: {item.vote}
                                         </p>
                                     </div>
-                                </div>
+                                </Link>
                             </div>
                         )
                     })
